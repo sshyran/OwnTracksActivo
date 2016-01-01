6e75079ee06d3752b9a5671076d8a572e5322888
Christoph Krey <krey.christoph@gmail.com>
2016-01-01 16:28:17 +0100

    Copyright 2016

--- OwnTracksActivo/IdPicker.m
+++ OwnTracksActivo/IdPicker.m
@@ -3,7 +3,7 @@
 //  Wegeheld
 //
 //  Created by Christoph Krey on 18.02.15.
-//  Copyright (c) 2015 OwnTracks. All rights reserved.
+//  Copyright © 2015-2016 OwnTracks. All rights reserved.
 //
 
 #import "IdPicker.h"
--- OwnTracksActivo/Task.m
+++ OwnTracksActivo/Task.m
@@ -3,7 +3,7 @@
 //  OwnTracksActivo
 //
 //  Created by Christoph Krey on 30.04.15.
-//  Copyright (c) 2015 OwnTracks. All rights reserved.
+//  Copyright © 2015-2016 OwnTracks. All rights reserved.
 //
 
 #import "Task.h"
